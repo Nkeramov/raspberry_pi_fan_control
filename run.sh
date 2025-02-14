cd07df22f1c8ecaf025649207fcd19c696d8690f
nizam <keramov-nizam@yandex.ru>
2025-02-14 16:42:47 +0300

    updated commands

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 source .venv/bin/activate
 # specify parameters as desired
-# python3 relay_controller.py --lower 35 --upper 40 --delay 5
-python3 proportional_controller.py --temp 45 --delay 5 --p 3 --dmin 30
+#python3 relay_controller.py --lower 40 --upper 45 --delay 5
+python3 proportional_controller.py --temp 45 --delay 5 --p 15.0 --dmin 55 --dmax 100
